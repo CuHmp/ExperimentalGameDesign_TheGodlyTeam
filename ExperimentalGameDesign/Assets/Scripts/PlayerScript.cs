bf6e98cf6b5f6249412114fb9c5054047d467a6d
Adam Olsson <adaols80@gmail.com>
2020-02-04 14:01:16 +0100

    Starting to implement player script and fixing git... plz work shit head

---
+++ ExperimentalGameDesign/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public KeyCode tilt_down_key;
+    public KeyCode tilt_up_key;
+
+    public KeyCode[] move_keys;
+
+    public GameObject bottle;
+
+    public Vector3 cursor;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
